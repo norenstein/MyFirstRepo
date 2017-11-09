cb6abac90d9082f1b207cb884dfe513df1495533
norenstein <33520761+norenstein@users.noreply.github.com>
2017-11-09 09:53:18 -0500

    Initial

---
+++ personality_NO.py
@@ -0,0 +1,22 @@
+name = "Nick"
+state = "California"
+book = "The Giver"
+city = "Chicago"
+tvshow = "sports center"
+
+videogame = "Ghost recon"
+movie = "Miracle"
+sportsteam = "Rangers"
+print(name + " likes to visit " + state + " and " + city)
+print (name + " watches " + tvshow + " and reads " + book)
+
+
+print("What's your favorite color?")
+color = input()\
+
+
+if color == "blue"
+    print("Blue is my favorite!")
+else:
+    print(color + " is nice too. " )
+
